7775788ae6630b28fa636ff229b51ca7e7bff3ae
schilukuri <schilukuri@gogoair.com>
2015-11-12 17:23:08 +0530

    Updated Code

--- SampleExample/HelloWorld.py
+++ SampleExample/HelloWorld.py
@@ -1,4 +1,4 @@
-friends=list(['SANDEEP','KIRAN','BARU',['JAVA','C#','PYTHON']])
+friends=list(['SANDEEP','KIRAN','BARU','SATYA','KOW','KIRANMI',['JAVA','C#','PYTHON']])
 for friend in friends:
     if isinstance(friend, list):
         for lang in range(0, len(friend)):
@@ -9,6 +9,9 @@ for friend in friends:
 # print(friends[0]) 
 # print(friends[1]) 
 # print(friends[2]) 
-# print(friends[3][0]) 
-# print(friends[3][1]) 
-# print(friends[3][2]) 
+# print(friends[3]) 
+# print(friends[4]) 
+# print(friends[5]) 
+# print(friends[6][0]) 
+# print(friends[6][1]) 
+# print(friends[6][2]) 
